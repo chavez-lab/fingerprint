7931a9a2bf7c5ea3e311697dd682fe18a5453a57
auberginekenobi <ochapman@ucsd.edu>
2020-07-13 17:31:06 -0700

    find local files

--- fingerprint.sh
+++ fingerprint.sh
@@ -6,15 +6,16 @@
 #	python 2.7
 #	pysam  0.7.8
 
+MYDIR="$(dirname "$(readlink -f "$0")")"
 BAM=$1
 REF=$2
 
 # Parse inputs
 SAMPLENAME=$(basename $BAM .bam)
 if [ $REF = "hg19" ]; then
-	BED="snp138Common.n1000.vh20140318.bed"
+	BED="$(<$MYDIR/snp138Common.n1000.vh20140318.bed)"
 elif [ $REF = "hg38" ]; then
-	BED="snp138Common.n1000.vh20140318.hg38.bed"
+	BED="$(<$MYDIR/snp138Common.n1000.vh20140318.hg38.bed)"
 else
 	echo "2nd arg should be 'hg19' or 'hg38'"
 	exit 1
